9972aa4033f7018b1584c6ba73c59960c2aac15a
Dmitry Ignatiev <lovesan.ru at gmail.com>
2011-12-13 13:23:17 +0400

    Adder more code to evaluator.

--- src/xi/eval.lisp
+++ src/xi/eval.lisp
@@ -21,11 +21,14 @@
                             (symbol-value '+global-var-info+)
                             (make-hash-table :test #'eq)))
 
+;; We really must implement this as a target lisp class.
 (defstruct (env (:constructor %make-env))
   vars
   tags
   blocks
-  cenv) ;; cenv is environment for macros and constants
+  cenv) ;; cenv is a `compiler environment'.
+        ;; Macros and constants are evaluated in this environment.
+       ; ; It holds macro/constant variable bindings.
 
 (defun make-env (&key parent vars tags blocks)
   (flet ((ct-vars (vars)
@@ -60,37 +63,59 @@
         :key #'car :test #'eq))
 
 (defun error-unbound-variable (symbol)
-  (eval-call [signal-error]
+  (eval-call (sym-value [signal-error])
              (list (sym-value [unbound-variable]) symbol)))
 
 (defun error-undeclared-variable (symbol)
-  (eval-call [signal-error]
+  (eval-call (sym-value [signal-error])
              (list (sym-value [undeclared-variable]) symbol)))
 
 (defun error-constant-modification (symbol)
-  (eval-call [signal-error]
+  (eval-call (sym-value [signal-error])
              (list (sym-value [constant-modification]) symbol)))
 
 (defun error-arg-count (args)
-  (eval-call [signal-error]
+  (eval-call (sym-value [signal-error])
              (list (sym-value [invalid-argument-count]) args)))
 
 (defun error-not-symbol (datum)
-  (eval-call [signal-error]
+  (eval-call (sym-value [signal-error])
              (list (sym-value [class-error])
                    datum
                    (sym-value [symbol]))))
+
+(defun error-not-list (datum)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [class-error])
+                   datum
+                   (sym-value [list]))))
+
+(defun error-not-function (datum)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [class-error])
+                   datum
+                   (sym-value [function]))))
+
+(defun error-not-callable (datum)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [instance-not-callable])
+                   datum)))
+
+(defun error-not-class-of (datum class)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [class-error])
+                   datum
+                   class)))
 
 (defun get-symbol-value (symbol)
   (let ((dynvar (find symbol *dynamic-vars*
                       :key #'car)))
     (if (not dynvar)
       (case (gethash symbol +global-var-info+)
-        ((:static :constant :macro :dynamic)
-         (if (eq (sym-value symbol) :unbound)
-           (error-unbound-variable symbol)
-           (sym-value symbol)))
-        (T (error-undeclared-variable symbol)))
+        ((:static :constant :macro) (sym-value symbol))
+        (T (if (eq (sym-value symbol) :unbound)
+             (error-unbound-variable symbol)
+             (sym-value symbol))))
       (if (eq :unbound (cdr dynvar))
         (error-unbound-variable symbol)
         (cdr dynvar)))))
@@ -100,16 +125,8 @@
                       :key #'car)))
     (if (not dynvar)
       (case (gethash symbol +global-var-info+)
-        ((:static :dynamic)
-         (setf (sym-value symbol) new-value))
-        (:macro (setf (sym-value symbol)
-                      (if (function-object-p new-value)
-                        new-value
-                        (alloc-fun
-                          :args (list [%form] [%env])
-                          :code (constantly new-value)))))
         (:constant (error-constant-modification symbol))
-        (T (error-undeclared-variable symbol)))
+        (T (setf (sym-value symbol) new-value)))
       (setf (cdr dynvar) new-value))))
 
 (defun eval-ref (symbol env)
@@ -117,22 +134,15 @@
     (if (null var)
       (case (gethash symbol +global-var-info+)
         (:dynamic (get-symbol-value symbol))
-        ((:static :constant)
-         (if (eq :unbound (sym-value symbol))
-           (error-unbound-variable symbol)
-           (sym-value symbol)))
-        (:macro (if (eq :unbound (sym-value symbol))
-                  (error-unbound-variable symbol)
-                  (eval-form (eval-macroexpand (sym-value symbol) symbol)
-                             env)))
+        ((:static :constant) (sym-value symbol))
+        (:macro (eval-form (sym-value symbol) env))
         (T (error-undeclared-variable symbol)))
       (ecase (second var)
         (:dynamic (get-symbol-value symbol))   
-        (:macro (eval-form (eval-macroexpand (cddr var) symbol)
-                           env))
+        (:macro (eval-form (cddr symbol) env))
         ((:static :constant) (cddr var))))))
 
-(defun eval-setq (form env)
+(defun %eval-setq (form env)
   (if (= (length form) 3)
     (if (symbol-object-p (second form))
       (let* ((symbol (second form))
@@ -141,10 +151,9 @@
         (if (null var)
           (case (gethash symbol +global-var-info+)
             (:dynamic (set-symbol-value symbol (eval-form value env)))
-            (:static (setf (sym-value symbol) (eval-form value env)))
             (:constant (error-constant-modification symbol))
             (:macro (eval-form `([setf] ,symbol ,value) env))
-            (T (error-undeclared-variable symbol)))
+            (T (setf (sym-value symbol) (eval-form value env))))
           (ecase (second var)
             (:dynamic (set-symbol-value symbol (eval-form value env)))
             (:static (setf (cddr var) (eval-form value env)))
@@ -153,17 +162,99 @@
       (error-not-symbol (second form)))
     (error-arg-count (rest form))))
 
-;; we pass `nil' here because XI lexenv structure
-;;  is not suitable for target lisp.
-;; TODO: make lexenv a lisp object. 
-(defun eval-macroexpand (expander form)
-  (let ((*dynamics* '()))
-    (eval-call expander (list form nil))))
+(defun eval-macroexpand (expander form lexenv)
+  (declare (ignore lexenv))
+  ;; NIL because our env structure is not a lisp instance(yet).
+  (eval-call expander (list form nil)))
 
 ;; Microlisp set of special operators is very similiar to that
 ;;  of Common Lisp, so in case of XI we reuse existing CL control structures
 ;;  and specifically, an implementation of NLX an dynamic environments.
 ;; Upcoming Microlisp runtime and compiler should implement all
 ;;   this stuff from scratch.
+
+(defun eval-cons (form env)
+  (case (car form)
+    ([lambda] (eval-lambda form env))
+    ([quote] (eval-quote form env))
+    ([setq] (eval-setq form env))
+    ([if] (eval-if form env))
+    (([toplevel-expansion-too]
+      [toplevel-expansion-only]
+      [body])
+     (eval-body form env))
+    ([body1] (eval-body1 form env))
+    ([let] (eval-let form env))
+    ([let*] (eval-let* form env))
+    ([letr] (eval-letr form env))
+    ([bind] (eval-bind form env))
+    ([catch] (eval-catch form env))
+    ([throw] (eval-throw form env))
+    ([block] (eval-block form env))
+    ([return-from] (eval-return-from env))
+    ([tagbody] (eval-tagbody form env))
+    ([go] (eval-go form env))
+    ([unwind-protect] (eval-unwind-protect form env))
+    ([mvcall] (eval-mvcall form env))
+    ([the] (eval-the form env))
+    (T (let ((f (car form)))
+         (if (symbol-object-p f)
+           (let ((var (env-var f)))
+             (if (null var)
+               (case (gethash f +global-var-info+)
+                 ((:static :constant)
+                  (eval-call (sym-value f) (eval-args (cdr form) env)))
+                 (:dynamic
+                  (eval-call (get-symbol-value f) (eval-args (cdr form) env)))
+                 (:macro (eval-macroexpand (sym-value f) form env))
+                 (T (error-undeclared-variable f)))
+               (ecase (second var)
+                 (:dynamic
+                  (eval-call (get-symbol-value f) (eval-args (cdr form) env)))
+                 ((:static :constant)
+                  (eval-call (cddr var) (eval-args (cdr form) env)))
+                 (:macro (eval-macroexpand (cddr var) form env)))))
+           (eval-call (eval-form (car form) env)
+                      (eval-args (cdr form) env)))))))
+
+(defun eval-args (args env)
+  (cond ((null args) '())
+        ((atom args) (let ((form (eval-form args env)))
+                       (if (listp form)
+                         form
+                         (error-not-list form env))))
+        (T (cons (eval-form (car args) env)
+                 (eval-args (cdr args) env)))))
+
+(defun eval-call (f args)
+#| TBD
+  (let* ((f (typecase f
+              (function-object f)
+              (instance-object (or (inst-function f)
+                                   (error-not-callable f)))
+              (T (error-not-function f))))
+         (required '())
+         (rest nil)
+         (*dynamic-vars* *dynamic-vars*))
+    (loop :for (p . rp) :on (fun-args f)
+          :for (a . ra) :on args
+          :if (not (is-p a (cddr p)))
+          :do (error-not-class-of a (cddr p))
+          :else if (eq :dynamic (cadr p))
+          :do (push (cons (car a) p) *dynamic-vars*)
+          :else :collect (list* (car p) (cadr p) a) :into req
+          :finally )
+  )
+|#
+  )
+
+(defun eval-form (form env)
+  (typecase form
+    (null nil)
+    (symbol-object (eval-ref form env))
+    (cons (eval-cons form env))
+    ((or character array number) form)
+    (T (lisp-runtime-error
+        "Invalid lisp object spotted: ~s" form))))
 
 (disable-xi-syntax)
