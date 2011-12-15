305135cb36b5c79564a5a1c933246476c4cd45dd
Dmitry Ignatiev <lovesan.ru at gmail.com>
2011-12-15 16:43:00 +0400

    Implemented few more special operators in XI.

--- src/xi/eval.lisp
+++ src/xi/eval.lisp
@@ -17,38 +17,22 @@
 
 (defvar *dynamic-vars* '())
 (defvar *catch-tags* '())
-(defvar +global-var-info+ (if (boundp '+global-var-info+)
-                            (symbol-value '+global-var-info+)
-                            (make-hash-table :test #'eq)))
+(defvar +global-var-info+ (make-hash-table :test #'eq))
 
 ;; We really must implement this as a target lisp class.
 (defstruct (env (:constructor %make-env))
   vars
   tags
-  blocks
-  cenv) ;; cenv is a `compiler environment'.
-        ;; Macros and constants are evaluated in this environment.
-       ; ; It holds macro/constant variable bindings.
+  blocks)
 
 (defun make-env (&key parent vars tags blocks)
-  (flet ((ct-vars (vars)
-           (remove-if
-            (lambda (var &aux (kind (cadr var)))
-              (not (or (eq kind :macro)
-                       (eq kind :constant))))
-            vars)))
-    (if (null parent)
-      (%make-env :vars vars
-                 :tags tags
-                 :blocks blocks
-                 :cenv (%make-env :vars (ct-vars vars)))
-      (let ((parent-vars (env-vars parent)))
-        (%make-env :vars (append vars parent-vars)
-                   :tags (append tags (env-tags parent))
-                   :blocks (append blocks (env-blocks parent))
-                   :cenv (%make-env
-                          :vars (append (ct-vars vars)
-                                        (env-vars (env-cenv parent)))))))))
+  (if (null parent)
+    (%make-env :vars vars
+               :tags tags
+               :blocks blocks)
+    (%make-env :vars (append vars (env-vars parent))
+               :tags (append tags (env-tags parent))
+               :blocks (append blocks (env-blocks parent)))))
 
 (defun env-var (env symbol)
   (find symbol (env-vars env)
@@ -103,6 +87,26 @@
                    datum
                    class)))
 
+(defun error-duplicate-doc (doc)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [duplicate-docstring])
+                   doc)))
+
+(defun error-invalid-decl (decl)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [invalid-declaration])
+                   decl)))
+
+(defun error-invalid-decl-context (decl)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [invalid-declaration-context])
+                   decl)))
+
+(defun error-dup-vardecl (decl)
+  (eval-call (sym-value [signal-error])
+             (list (sym-value [duplicate-variable-declaration])
+                   decl)))
+
 (defun get-symbol-value (symbol)
   (let ((dynvar (find symbol *dynamic-vars*
                       :key #'car)))
@@ -130,46 +134,157 @@
     (if (null var)
       (case (gethash symbol +global-var-info+)
         (:dynamic (get-symbol-value symbol))
-        ((:static :constant) (sym-value symbol))
-        (:macro (eval-form (sym-value symbol) env))
+        (:static (sym-value symbol))
         (T (if (eq :unbound (sym-value symbol))
              (error-unbound-variable symbol)
              (sym-value symbol))))
       (ecase (second var)
-        (:dynamic (get-symbol-value symbol))   
-        (:macro (eval-form (cddr symbol) env))
-        ((:static :constant) (cddr var))))))
+        (:dynamic (get-symbol-value symbol))
+        (:static (cddr var))))))
 
-(defun %eval-setq (form env)
-  (if (= (length form) 3)
-    (if (symbol-object-p (second form))
-      (let* ((symbol (second form))
-             (value (third form))
-             (var (env-var env symbol)))
-        (if (null var)
-          (case (gethash symbol +global-var-info+)
-            (:dynamic (set-symbol-value symbol (eval-form value env)))
-            (:constant (error-constant-modification symbol))
-            (:macro (eval-form `([setf] ,symbol ,value) env))
-            (T (setf (sym-value symbol) (eval-form value env))))
-          (ecase (second var)
-            (:dynamic (set-symbol-value symbol (eval-form value env)))
-            (:static (setf (cddr var) (eval-form value env)))
-            (:constant (error-constant-modification symbol))
-            (:macro (eval-form `([setf] ,symbol ,value) env)))))
-      (error-not-symbol (second form)))
-    (error-arg-count (rest form))))
+(defun %eval-setq (symbol value env)
+  (let ((var (env-var env symbol)))
+    (if (null var)
+      (case (gethash symbol +global-var-info+)
+        (:dynamic (set-symbol-value symbol (eval-form value env)))
+        (:constant (error-constant-modification symbol))
+        (:macro (eval-form `([setf] ,symbol ,value) env))
+        (T (setf (sym-value symbol) (eval-form value env))))
+      (ecase (second var)
+        (:dynamic (set-symbol-value symbol (eval-form value env)))
+        (:static (setf (cddr var) (eval-form value env)))
+        (:constant (error-constant-modification symbol))
+        (:macro (eval-form `([setf] ,symbol ,value) env))))))
 
-(defun eval-macroexpand (expander form lexenv)
-  (declare (ignore lexenv))
-  ;; NIL because our env structure is not a lisp instance(yet).
-  (eval-call expander (list form nil)))
+(defun expand (form env)
+  (cond
+    ((symbol-object-p form)
+     (let ((var (env-var form)))
+       (if (null var)
+         (case (gethash form +global-var-info+)
+           (:macro (expand (sym-value form)))
+           (:constant (sym-value form))
+           (t form))
+         (ecase (second var)
+           (:macro (expand (cddr var)))
+           (:constant (cddr var))
+           (t form)))))
+    ((consp form)
+     (if (symbol-object-p (car form))
+       (let ((var (env-var (car form))))
+         (if (null var)
+           (case (gethash (car form) +global-var-info+)
+             (:macro (expand (eval-call
+                              (sym-value (car form))
+                              (list form env))))
+             (T form))
+           (ecase (second var)
+             (:macro (expand (eval-call
+                              (cddr var)
+                              (list form env))))
+             (T form))))
+       form))
+    (T form)))
+
+(defun parse-body (body &key allow-doc allow-locals allow-macros)
+  (multiple-value-bind
+        (doc decls forms)
+      (loop :with doc = nil
+            :with decls = '()
+            :for form :on body :do
+            (cond ((and allow-doc (stringp (car form)))
+                   (if doc
+                     (error-duplicate-doc (car form))
+                     (setf doc (car form))))
+                  ((and (consp form) (eq [declare] (caar form)))
+                   (setf decls (append decls (cdar form))))
+                  (T (return (values doc decls form))))
+            :finally (return (values doc decls nil)))
+    (let (statics dynamics constants macros names)
+      (dolist (d decls)
+        (if (consp d)
+          (macrolet ((foo (vtype)
+                       (let ((v (gensym))
+                             (r (gensym)))
+                         `(loop :for  (,v . ,r) :on (cdr d)
+                                :if (null ,r) :do (return)
+                                :else :if (atom ,r)
+                                :do (error-not-list d)
+                                :else :if (find ,v names)
+                                :do (error-dup-vardecl d)
+                                :else :do
+                                (push ,v names)
+                                (push ,v ,vtype)))))
+            (case (car d)
+              ([dynamic] (foo dynamics))
+              ([static] (if allow-locals
+                          (foo statics)
+                          (error-invalid-decl-context d)))
+              ([constant] (if allow-macros
+                            (foo constants)
+                            (error-invalid-decl-context d)))
+              ([macro] (if allow-macros
+                         (foo macros)
+                         (error-invalid-decl-context d)))))
+          (error-invalid-decl d)))
+      (values forms doc statics dynamics constants macros))))
 
 ;; Microlisp set of special operators is very similiar to that
 ;;  of Common Lisp, so in case of XI we reuse existing CL control structures
 ;;  and specifically, an implementation of NLX an dynamic environments.
 ;; Upcoming Microlisp runtime and compiler should implement all
 ;;   this stuff from scratch.
+
+(defun eval-quote (form env)
+  (declare (ignore env))
+  (if (= 2 (length form))
+    (second form)
+    (error-arg-count (cdr form))))
+
+(defun eval-setq (form env)
+  (let ((pairs (rest form)))
+    (if (evenp (length pairs))
+      (let (last)
+        (loop :for (s v) :on pairs :by #'cddr
+              :if (symbol-object-p s)
+              :do (setf last (%eval-setq s v env))
+              :else :do (error-not-symbol s))
+        last)
+      (error-arg-count pairs))))
+
+(defun eval-if (form env)
+  (case (length form)
+    ((3 4) (if (eval-form (second form) env)
+             (eval-form (third form) env)
+             (eval-form (fourth form) env)))
+    (T (error-arg-count (rest form)))))
+
+(defun eval-progn (forms env)
+  (let (prev)
+    (dolist (expr forms)
+      (when prev (eval-form prev env))
+      (setf prev expr))
+    (eval-form prev env)))
+
+(defun eval-body (form env)
+  (multiple-value-bind
+        (forms doc statics dynamics constants macros)
+      (parse-body (rest form))
+    (declare (ignore doc statics constants macros))
+    (if (endp dynamics)
+      (eval-progn forms env)
+      (let ((env (make-env :parent env
+                           :vars (mapcar (lambda (v)
+                                           (cons v :dynamic))
+                                         dynamics))))
+        (eval-progn forms env)))))
+
+(defun eval-the (form env)
+  (if (= 3 (length form))
+    ;; I'm too lazy to implement this.
+    ;; TODO: perform type checking
+    (eval-form (third form) env)
+    (error-arg-count (rest form))))
 
 (defun eval-cons (form env)
   (case (car form)
@@ -195,27 +310,8 @@
     ([unwind-protect] (eval-unwind-protect form env))
     ([mvcall] (eval-mvcall form env))
     ([the] (eval-the form env))
-    (T (let ((f (car form)))
-         (if (symbol-object-p f)
-           (let ((var (env-var f)))
-             (if (null var)
-               (case (gethash f +global-var-info+)
-                 ((:static :constant)
-                  (eval-call (sym-value f) (eval-args (cdr form) env)))
-                 (:dynamic
-                  (eval-call (get-symbol-value f) (eval-args (cdr form) env)))
-                 (:macro (eval-macroexpand (sym-value f) form env))
-                 (T (if (eq :unbound (sym-value f))
-                      (error-unbound-variable f)
-                      (eval-call (sym-value f) (eval-args (cdr form) env)))))
-               (ecase (second var)
-                 (:dynamic
-                  (eval-call (get-symbol-value f) (eval-args (cdr form) env)))
-                 ((:static :constant)
-                  (eval-call (cddr var) (eval-args (cdr form) env)))
-                 (:macro (eval-macroexpand (cddr var) form env)))))
-           (eval-call (eval-form (car form) env)
-                      (eval-args (cdr form) env)))))))
+    (T (eval-call (eval-form (car form) env)
+                  (eval-args (cdr form) env)))))
 
 (defun eval-args (args env)
   (cond ((null args) '())
@@ -249,12 +345,22 @@
   )
 
 (defun eval-form (form env)
-  (typecase form
-    (null nil)
-    (symbol-object (eval-ref form env))
-    (cons (eval-cons form env))
-    ((or character array number) form)
-    (T (lisp-runtime-error
-        "Invalid lisp object spotted: ~s" form))))
+  (let ((form (expand form env)))
+    (typecase form
+      (null nil)
+      (symbol-object (eval-ref form env))
+      (cons (eval-cons form env))
+      ((or character array number) form)
+      (T (lisp-runtime-error
+          "Invalid lisp object spotted: ~s" form)))))
+
+(defun eval-form-in-cenv (form env)
+  (let ((cenv (make-env
+               :vars (loop :for v :in (env-vars env)
+                           :for vt = (second v)
+                           :when (or (eq vt :constant)
+                                     (eq vt :macro))
+                           :collect v))))
+    (eval-form form cenv)))
 
 (disable-xi-syntax)
